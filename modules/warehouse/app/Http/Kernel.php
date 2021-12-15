47f6cd797f874be6c76e635a4f19d675743006c4
Leonardo Salazar <leozxxz@gmail.com>
2021-12-15 02:17:13 -0500

    feat(warehouse): Added a middleware to make the app always return JSO

--- modules/warehouse/app/Http/Kernel.php
+++ modules/warehouse/app/Http/Kernel.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $middleware = [
+        \App\Http\Middleware\JsonMiddleware::class,
         \App\Http\Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
