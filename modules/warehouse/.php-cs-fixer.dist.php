3320fc72c1d5296dc75b1c69c2ff14c88f5eee65
Leonardo Salazar <leozxxz@gmail.com>
2021-12-14 22:37:49 -0500

    build(warehouse): Added PHP CS Fixer and PHPStan

---
+++ modules/warehouse/.php-cs-fixer.dist.php
@@ -0,0 +1,33 @@
+<?php
+
+/** @noinspection DevelopmentDependenciesUsageInspection */
+return (new PhpCsFixer\Config())
+    ->setRules(
+        [
+            '@PSR12' => true,
+            '@PHP81Migration' => true,
+            '@PhpCsFixer' => true,
+            'concat_space' => ['spacing' => 'one'],
+            'yoda_style' => false,
+        ]
+    )
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in(__DIR__)
+            ->exclude(
+                [
+                    'bootstrap/cache',
+                    'config',
+                    'public',
+                    'resources/lang',
+                    'storage',
+                    'vendor',
+                ],
+            )
+            ->notPath(
+                [
+                    'server.php',
+                ],
+            )
+    )
+;
